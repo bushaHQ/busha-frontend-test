acfbd50039500921dd7db09354852c197c36bc71
Ogheneovo Eko <ogheneovo.eko@geocloud.co>
2022-02-12 23:39:38 +0100

    feat: heroku deploy changes reverted for submitted branch

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults({ static: "./build" });
+const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 3090;
+const PORT = process.env.SERVER_PORT || 3090;
 
 server.use(middlewares);
 
