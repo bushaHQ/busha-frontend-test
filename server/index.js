91509e9ceabd78c7f75604a72edebcf11f70c1cb
Ogheneovo Eko <ogheneovo.eko@geocloud.co>
2022-02-12 21:19:54 +0100

    feat: heroku deploy attempt

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ static: "./build" });
 
 const PORT = process.env.SERVER_PORT || 3090;
 
