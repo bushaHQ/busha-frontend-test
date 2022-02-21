a389f50a84e8aa99880b6ec9994f23be8e7af2b8
Michael Chilaka <obichilaka@gmail.com>
2022-02-21 22:38:40 +0100

    updates

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,91 +1,71 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import makeAPICall from '../utils/config';
 import Loader from "../components/shared/Loader";
 
 import "../App.scss";
 
+interface ItemsProps {
+    currency: string,
+    name: string,
+    imgURL: string,
+    type: string
+}
 
 const Dashboard = () => {
 
-    const [walletItems, setWalletItems] = useState([]);
+    const [walletItems, setWalletItems] = useState<ItemsProps[]>();
     const [hasError, setHasError] = useState("");
+    const [isLoading, setIsLoading] = useState(false)
 
     const getWallets = () => {
+        setIsLoading(true)
+        setHasError("");
         return makeAPICall({
-          path: "wallets",
-          method: "GET",
+            path: "wallets",
+            method: "GET",
         })
-          .then((data) => {
-              console.log(data);
-              
-            setWalletItems(data.data);
-            // setTable(true);
-          })
-          .catch((err) => {
-              setHasError(err.message)
-              console.log(err)
+            .then((data) => {
+                console.log(data, "sam");
+                setIsLoading(false)
+                setHasError("");
+                setWalletItems(data);
+            })
+            .catch((err) => {
+                setHasError(err.message)
+                setIsLoading(false)
+                console.log(err)
             });
-      };
-    
-      useEffect(() => {
-        getWallets();
-      }, []);
+    };
 
-    // const getWalletData = () => {
-    //     fetch("http://localhost:3090/wallets", {
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //         "Accept": "application/json"
-    //       }
-    //     }).then(function (res) {
-    //       return res.json()
-    //     }).then(function (myJson) {
-    //       console.log(myJson, "ee");
-    //       setWalletItems(myJson)
-    //     })
-    //   }
-    
-    //   useEffect(()=>{
-    //     getWalletData()
-    //   },[])
+    useEffect(() => {
+        getWallets();
+    }, []);
     return (
         <>
             <div className="dashboard__main__content">
                 <div className="wallets__main">
                     <h1>Wallets</h1>
-                    <Loader />
+                    <p>{hasError ? hasError : ""}</p>
                     {
+                        isLoading ? (<Loader />) : (
+                            <div className='wallets__main__row'>
+                                {
+                                    walletItems?.length === 0 ? (
+                                        "No results found"
+                                    ) : walletItems?.map((items, i) => {
+                                        <div className='wallets__main__column' key={i}>
+                                            <div className='wallets__main__card'>
+                                                <span></span>
+                                                <p>{items.name}</p>
+                                            </div>
+                                        </div>
+                                        // console.log(items.name);
 
+                                    })}
+                                eee
+                            </div>
+                        )
                     }
-                    <div className='wallets__main__row'>
-                        <div className='wallets__main__column'>
-                            <div className='wallets__main__card'>
-                                <span></span>
-                                <p>₦ 105,160,076.51</p>
-                            </div>
-                        </div>
-
-                        <div className='wallets__main__column'>
-                            <div className='wallets__main__card'>
-                                <span></span>
-                                <p>₦ 105,160,076.51</p>
-                            </div>
-                        </div>
-
-                        <div className='wallets__main__column'>
-                            <div className='wallets__main__card'>
-                                <span></span>
-                                <p>₦ 105,160,076.51</p>
-                            </div>
-                        </div>
-
-                        <div className='wallets__main__column'>
-                            <div className='wallets__main__card'>
-                                <span></span>
-                                <p>₦ 105,160,076.51</p>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </>
