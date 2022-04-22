f9bab00e54f1e0adaaf94abc942919c8e662de7f
nonsoo24 <daniel.chukwurah@gmail.com>
2022-04-22 20:39:48 +0100

    chore: add icon and image

---
+++ src/assets/icons/ArrowRightIcon.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import { SVGProps } from "react"
+
+const ArrowRightIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width={32}
+    height={32}
+    fill="none"
+    {...props}
+  >
+    <rect width={32} height={32} rx={16} fill="#303030" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M14 22a.999.999 0 0 1-.707-1.707L17.586 16l-4.293-4.293a.999.999 0 1 1 1.414-1.414l5 5a.999.999 0 0 1 0 1.414l-5 5A.997.997 0 0 1 14 22Z"
+      fill="#CBD2D9"
+    />
+    <mask
+      id="a"
+      style={{
+        maskType: "alpha",
+      }}
+      maskUnits="userSpaceOnUse"
+      x={12}
+      y={9}
+      width={9}
+      height={13}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M14 22a.999.999 0 0 1-.707-1.707L17.586 16l-4.293-4.293a.999.999 0 1 1 1.414-1.414l5 5a.999.999 0 0 1 0 1.414l-5 5A.997.997 0 0 1 14 22Z"
+        fill="#fff"
+      />
+    </mask>
+  </svg>
+)
+
+export default ArrowRightIcon
