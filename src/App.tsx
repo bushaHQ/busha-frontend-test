abc4f92ad24987c35215db2519c55ae201da27b9
Ridwan Abiola <ricklick262@gmail.com>
2022-11-01 08:37:50 +0100

    feat: dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,51 @@ import "./App.scss";
 import { NavBar } from "./globals/NavBar";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div className="App">
+      <NavBar />
+      <div className="flex container dashboard">
+        <Sidebar />
+        <Dashboard />
+      </div>
+    </div>
+  );
 }
+
+interface SidebarProps {}
+const Sidebar: React.FC<SidebarProps> = (props) => {
+  return (
+    <aside className="flex flow sidebar">
+      <nav className="fh-screen">
+        <ul className="flow">
+          <li>
+            <a href="">Wallets</a>
+          </li>
+          <li>
+            <a href="">Prices</a>
+          </li>
+          <li>
+            <a href="">Peer2Peer</a>
+          </li>
+          <li>
+            <a href="">Activity</a>
+          </li>
+          <li>
+            <a href="">Settings</a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+interface DashboardProps {}
+const Dashboard: React.FC<DashboardProps> = (props) => {
+  return (
+    <main>
+      <h1>Wallets</h1>
+    </main>
+  );
+};
 
 export default App;
