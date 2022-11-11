bf0ab78440b7cb532ebd0c3436065bb7860e040d
horlard <oladipuposaulih@gmail.com>
2022-11-11 22:29:06 +0100

    update in directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Header from '../src/components/shared/Header'
-import MainSection from './MainSection';
+import MainSection from './utils/MainSection';
 
 function App() {
   return (
