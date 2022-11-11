ad60f9ccc0ad3ddd249a12ca7610adf38cfe07ca
horlard <oladipuposaulih@gmail.com>
2022-11-11 22:21:35 +0100

    update

---
+++ src/utils/CreateWallet.tsx
@@ -0,0 +1,158 @@
+import React,{ useEffect,useState} from 'react'
+import styled from 'styled-components'
+
+import {ReactComponent as CloseIcon} from '../assets/svgs/close.svg'
+import {ReactComponent as FlagIcon} from '../assets/svgs/flag.svg'
+import SelectInput from '../components/shared/SelectInput'
+import Button from '../components/shared/Button'
+import WalletApi from '../api/Wallets-api-config'
+import { walletList } from '../types'
+import Loader from '../components/shared/Loader'
+import Error from '../components/shared/Error'
+
+interface Iprops {
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+  accountReload:React.Dispatch<React.SetStateAction<boolean>>
+}
+
+
+const CreateWallet:React.FC<Iprops> = ({setShowModal,accountReload}) => {
+    const [wallets,setWallets] = useState<walletList[]>([])
+    const [isLoading,setIsLoading] = useState<boolean| null>(true)
+    const [reload,setReload]=useState<boolean>(false)
+    const [selectedWallet,setSelectedWallet] = useState<string>('')
+    const [showErrMessage,setShowErrMessage] = useState<boolean>(false)
+
+    useEffect(()=> {
+        setIsLoading(true)
+        const fetchData=async()=>{
+            const response= await WalletApi.get('/wallets')
+            console.log(response.data);
+            setIsLoading(false)
+            setWallets(response.data)
+        }
+        fetchData().catch(()=>setIsLoading(null))
+    },[reload])
+
+    const componentRender=()=> {
+        if(isLoading) {
+          return (
+            <LoaderContainer>
+                <Loader size={40} width={3}/>
+            </LoaderContainer> 
+          )
+        }else if(isLoading===false) {
+          return (
+            <>
+                <p>The crypto wallet will be created instantly and be available in your list of wallets.</p>
+                <SelectInput wallets={wallets} onSelectChange={onSelectChange} selectedWallet={selectedWallet}/>
+                <Button title='Create a Wallet' onClickEvent={onSubmitWallet}/>
+                {
+                  showErrMessage ? <ErrorMessage>
+                  <FlagIcon/>
+                  <p>
+                  No more available wallet
+                  </p>
+                </ErrorMessage> : null
+                }
+                
+            </>
+          )
+        }else{
+          return (
+            <ErrorContainer>
+                <Error setReload={setReload}/>
+            </ErrorContainer>
+          )
+        }
+      }
+
+    const onSelectChange=(e:React.ChangeEvent<HTMLInputElement>)=> {
+        setSelectedWallet(e.target.value)
+        console.log(selectedWallet);
+        
+    }
+    const onSubmitWallet=()=> {
+      // console.log('hey');
+      
+      WalletApi.post('/accounts',{
+        currency: selectedWallet
+      }).then(()=> overallReload()).catch(()=>setShowErrMessage(true)
+      )
+        
+    }
+
+    const overallReload=()=> {
+      accountReload(prevState=>!prevState)
+      setReload(prevState=>!prevState)
+      setShowModal(false)
+    }
+  return (
+    <>
+         <Container>
+         <Header>
+              <p>Add new Wallet</p>
+              <CloseIcon onClick={()=>setShowModal(false)}/>
+          </Header>
+          {componentRender()}
+        </Container>
+    </>
+   
+  )
+}
+
+const Container = styled.div`
+    padding: 20px 30px;
+    display: flex;
+    flex-direction: column;
+    &>p {
+        font-size: 15px;
+        font-weight: 400;
+        color:#3E4C59;
+        line-height: 23px;
+        padding-bottom: 20px;
+    }
+`
+const Header=styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    &>p {
+        font-weight: 600;
+        font-size: 18px;
+    }
+    &>svg {
+      cursor: pointer;
+    }
+`
+
+const LoaderContainer= styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 82%;
+`
+const ErrorContainer=styled.div`
+    padding-top: 52%;
+`
+const ErrorMessage=styled.div`
+  display: flex;
+  font-size: 14px;
+  font-weight: 600;
+  align-items: center;
+  color: #D72C0D;
+  width: 96%;
+  gap: 10px;
+  background: #FFF4F4;
+  border: 1px solid #E0B3B2;
+  padding-left: 15px;
+  border-radius: 8px;
+  margin-top: 10%;
+  &>p {
+    
+  }
+`
+// const ErrInfo=styled.div`
+  
+// `
+
+export default CreateWallet;
---
+++ src/utils/MainSection.tsx
@@ -0,0 +1,140 @@
+import React,{useState,useEffect} from 'react'
+import styled from 'styled-components'
+
+import WalletCard from '../components/WalletCard'
+import WalletApi from '../api/Wallets-api-config'
+import { walletInfo } from '../types'
+import Loader from '../components/shared/Loader'
+import Error from '../components/shared/Error'
+import Modal from '../components/shared/Modal'
+import CreateWallet from './CreateWallet'
+
+const MainSection = () => {
+  const [accounts,setAccounts]= useState<walletInfo[]>([]);
+  const [reload,setReload]=useState<boolean>(false)
+  const [isLoading,setIsLoading]=useState<boolean | null>(true)
+  const [showModal,setShowModal]= useState<boolean>(false)
+
+
+  useEffect(()=> {
+    setIsLoading(true)
+    const fetchData= async()=> {
+      const response=await WalletApi.get('/accounts')
+      setAccounts(response.data)
+      setIsLoading(false)
+      console.log(response.data);
+    }
+    fetchData().catch(()=> {
+      setIsLoading(null)
+    })
+  },[reload])
+
+  const conditionRendering=()=> {
+    if(isLoading) {
+      return (
+        <LoaderContainer>
+          <Loader size={40} width={3}/>
+        </LoaderContainer> 
+      )
+    }else if(isLoading===false) {
+      return (
+        <Wallets>
+              {
+                accounts.map(acct=> <WalletCard account={acct} key={acct.id}/>)
+              }
+          </Wallets>
+      )
+    }else{
+      return (
+        <ErrorContainer>
+            <Error setReload={setReload}/>
+        </ErrorContainer>
+      )
+    }
+  }
+
+  return (
+      <MainContainer>
+        <Modal isOpen={!showModal ? false : true}>
+          <CreateWallet setShowModal={setShowModal} accountReload={setReload}/>
+        </Modal>
+          <RouteList>
+            <li>Wallets</li>
+            <li>Prices</li>
+            <li>Peer2Peer</li>
+            <li>Activity</li>
+            <li>Settings</li>
+          </RouteList>
+          <WalletContent>
+            <WalletHeader>
+              <h2>Wallets</h2>
+              <p onClick={()=>setShowModal(true)}><span>+</span> &nbsp;Add new wallet</p>
+            </WalletHeader>
+            {conditionRendering()}
+          </WalletContent>
+      </MainContainer>
+  )
+}
+
+const MainContainer= styled.div`
+  display: flex;
+  align-items: flex-start;
+  padding: 2% 8%;
+`
+const RouteList=styled.ul`
+  list-style-type: none;
+  flex-basis: 18%;
+  position: sticky;
+  &>li {
+    font-size: 14px;
+    font-weight: 400;
+    padding: 13px 20px 13px 13px;
+    color:#3E4C59;
+    cursor: pointer;
+  }
+  &>li:nth-child(1) {
+    background-color:  #F5F7FA;
+    border-radius: 3px;
+    color: #000;
+    font-weight: 600;
+  }
+`
+const WalletContent=styled.div`
+    padding-left: 5%;
+    flex-grow: 1;
+    overflow-y: auto;
+`
+const LoaderContainer=styled.div`
+      display: flex;
+      justify-content: center;
+      padding-top: 10%;
+`
+const ErrorContainer=styled.div`
+  padding-top: 5%;
+`
+const WalletHeader=styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid rgba(211, 213, 216, 0.5);
+  &>p {
+      color: #000;
+      font-weight: 600;
+      font-size: 14px;
+      cursor: pointer;
+    &>span {
+      margin-left: 2px;
+    }
+  }
+`
+const Wallets=styled.div`
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: repeat(3,1fr);
+  gap: 40px;
+  padding-top: 2%;
+
+`
+
+
+export default MainSection;
