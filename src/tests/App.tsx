0a7a80bc1ee20808b5891b62681ab337a802ab99
Adedeji Adeoluwa <135717991+adeoluwajohn01@users.noreply.github.com>
2023-07-10 14:22:02 +0100

    Add files via upload
    adding the typescript files to src folder

---
+++ src/tests/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Loader from './components/Loader';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Loader />
+    </>
+  );
+}
+
+export default App;
