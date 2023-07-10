ecc65e4d0b71cd0789d14674d721cac0ce2adfd1
Adedeji Adeoluwa <135717991+adeoluwajohn01@users.noreply.github.com>
2023-07-10 14:32:43 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import Navbar from './components/shared/Navbar';
 import Cards from './components/shared/Cards';
-import Card from './components/shared/Card;
-import Loader from './components/shared/Loader;
-import Sidebar from './components/shared/Sidebar;
+import Card from './components/shared/Card';
+import Loader from './components/shared/Loader';
+import Sidebar from './components/shared/Sidebar';
 function App() {
   return (
     <>
